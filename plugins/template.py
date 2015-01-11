e0725bd1c1e2f5658aa8206a0f0123a0486d024e
jurassicplayer <jurassicplayer75@gmail.com>
2015-01-11 01:38:16 -0800

    Changed to match function names in ircClient.py

--- plugins/template.py
+++ plugins/template.py
@@ -2,24 +2,23 @@
 
 class IRCScript:
     def __init__(self, client):
-        self.sendMsg = client.sendMsg
-        self.sendNotice = client.sendNotice
-        self.sendAction = client.sendAction
-        self.sendKick = client.sendKick
+        self.sendMsg = client.privmsg
+        self.sendNotice = client.notice
+        self.sendAction = client.action
+        self.sendKick = client.kick
         self.sendIRC = client.sendIRC
         self.join = client.join
         self.part = client.part
-        self.nick = client.NICK
-        self.pollNick = client.pollNick
-        self.channel = client.CHAN
+        self.NICK = client.NICK
     def privmsg(self, user, channel, msg): pass
     def action(self, user, channel, msg): pass
     def notice(self, user, msg): pass
     def joined(self, user, channel): pass
     def userJoined(self, user, channel): pass
-    def nickSwap(self, user, newnick, channel): pass
+    def nick(self, user, newnick, channel): pass
     def left(self, user, channel): pass
     def userLeft(self, user, channel): pass
     def userQuit(self, user, quitMessage): pass
+    def kicked(self, channel, kicker, msg): pass
     def userKicked(self, kickee, channel, kicker, msg): pass
     def userMode(self, mod, user, channel, mode): pass
