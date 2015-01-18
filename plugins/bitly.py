ea43bd44f6b64b3826abbcd63f35d72efbd3853b
jurassicplayer <jurassicplayer75@gmail.com>
2015-01-18 03:41:47 -0800

    Initial commit. Placeholder.

---
+++ plugins/bitly.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import template
+import re
+import pycurl
+import config
+
+class bitlyManager:
+    def retrieve_auth():
+        print('')
+        
+
+class IRCScript(template.IRCScript):
+    print('loaded bitly')
+    def privmsg(self, user, channel, msg):
+        print('')
