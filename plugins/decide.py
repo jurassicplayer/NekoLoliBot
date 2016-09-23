748e74b7af14b3d306f14e6902cac7c7396c564f
jurassicplayer <jurassicplayer75@gmail.com>
2016-09-22 22:29:47 -0700

    Testing methods of persistent data

--- plugins/decide.py
+++ plugins/decide.py
@@ -3,11 +3,18 @@
 import template
 import re, random
 
+class Persistent:
+    def __init__(self):
+        self.global_result = 'hurrdurr'
+        self.local_result = 'derp perp'
+persist = Persistent()
+
+
 class IRCScript(template.IRCScript):
-    print('loaded decide')
-    def privmsg(self, user, channel, msg):
+    global persist
+    def privmsg(self, origin, target, msg):
         decisions = re.match('^-decide\s(?P<choices>.*)', msg, re.I)
         if decisions:
             choices = decisions.group('choices').split('|')
             result = random.choice(choices)
-            self.sendMsg(channel, "I'd go with "+result)
+            self.sendMsg(target, "I'd go with "+result)
